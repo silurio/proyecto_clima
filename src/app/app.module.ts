f3c4b17d938ffd436fa2324f19379449148a603c
Sergio MeNu <silurio@live.com>
2023-06-14 11:50:48 -0600

    Se agrega tailwind al proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+// import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,7 +10,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // TranslateModule.forRoot({
+    //   defaultLanguage: 'es',
+    //   loader: {
+    //       provide: TranslateLoader,
+    //       useFactory: (http: HttpClient) => new TranslateHttpLoader(http, './assets/i18n/', '.json'),
+    //       deps: [HttpClient]
+    //   }
+    // })
   ],
   providers: [],
   bootstrap: [AppComponent]
