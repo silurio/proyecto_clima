8f3c7e99ec7c9798f2b3b851bf49a2d5b6d42cda
Sergio MeNu <silurio@live.com>
2023-06-14 22:29:59 -0600

    feat(weather.service.ts): Se agrega service para consulta de la API de Open Weather

---
+++ src/app/shared/services/weather.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+
+  private API_KEY = '2fdc9e8ee304182e483d77c29347896a';
+  private FORECAST_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${this.API_KEY}`;
+  private WEATHER_URL = `http://api.openweathermap.org/data/2.5/weather?appid=${this.API_KEY}`;
+  private AIR_POLLUTION_URL = `http://api.openweathermap.org/data/2.5/air_pollution?`;
+  constructor(private http: HttpClient) { }
+
+  getForecast(city: string): Observable<any> {
+    const url = `${this.FORECAST_URL}&q=${city}`;
+    return this.http.get(url);
+  }
+
+  getWeather(city: string): Observable<any> {
+    const url = `${this.WEATHER_URL}&q=${city}`;
+    return this.http.get(url);
+  }
+
+  getAirPollution(latitude: string, longitude: string): Observable<any> {
+    const url = `${this.AIR_POLLUTION_URL}lat=${latitude}&lon=${longitude}&appid=${this.API_KEY}`;
+    return this.http.get(url);
+  }
+}
