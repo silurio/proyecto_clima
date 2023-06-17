001870bfc7368205f22969aef706c0a229dac7bb
Sergio MeNu <silurio@live.com>
2023-06-17 05:20:21 -0600

    feat(ModalComponent): Se agregó modal de espera al procesado de las peticiones a la API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     HttpClientModule,
     TranslateModule.forRoot({
--- src/app/modules/usa-capitals-weather/usa-capitals-weather.module.ts
+++ src/app/modules/usa-capitals-weather/usa-capitals-weather.module.ts
@@ -1,21 +1,29 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
-import { UsaCapitalsWeatherRoutingModule } from './usa-capitals-weather-routing.module';
-import { UsaCapitalsWeatherComponent } from './components/usa-capitals-weather/usa-capitals-weather.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { TranslateModule } from '@ngx-translate/core';
+import { ModalComponent } from './components/modal/modal.component';
+import { UsaCapitalsWeatherComponent } from './components/usa-capitals-weather/usa-capitals-weather.component';
+import { UsaCapitalsWeatherRoutingModule } from './usa-capitals-weather-routing.module';
+import { OverlayModule } from '@angular/cdk/overlay';
 
 
 @NgModule({
   declarations: [
-    UsaCapitalsWeatherComponent
+    UsaCapitalsWeatherComponent,
+    ModalComponent
   ],
   imports: [
     CommonModule,
     ReactiveFormsModule,
     TranslateModule,
-    UsaCapitalsWeatherRoutingModule
+    OverlayModule,
+    UsaCapitalsWeatherRoutingModule,
+    MatProgressSpinnerModule,
+    MatDialogModule
   ]
 })
 export class UsaCapitalsWeatherModule { }
