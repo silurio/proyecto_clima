00acaff1a3572c98ed9ae0a90d40e35a8aad6078
Sergio MeNu <silurio@live.com>
2023-06-18 02:04:07 -0600

    feat(weather.service): Se agrega la API key al environment

--- src/app/shared/services/weather.service.ts
+++ src/app/shared/services/weather.service.ts
@@ -1,17 +1,20 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
 
-  private API_KEY = '2fdc9e8ee304182e483d77c29347896a';
+  private API_KEY = environment.apiKey;
   private FORECAST_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${this.API_KEY}`;
   private WEATHER_URL = `http://api.openweathermap.org/data/2.5/weather?appid=${this.API_KEY}`;
   private AIR_POLLUTION_URL = `http://api.openweathermap.org/data/2.5/air_pollution?`;
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient
+  ) { }
 
   getForecast(city: string): Observable<any> {
     const url = `${this.FORECAST_URL}&q=${city}`;
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: '2fdc9e8ee304182e483d77c29347896a'
 };
 
 /*
