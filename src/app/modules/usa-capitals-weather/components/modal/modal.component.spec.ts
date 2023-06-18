f64a02932a9e39ecba90b27afb0d7eb8fa866c6b
Sergio MeNu <silurio@live.com>
2023-06-18 03:56:18 -0600

    fix(ModalComponent): Se modifica el módulo en el que se alberga al componente y se modifican los imports en la prueba para resolver error NG0302

--- src/app/modules/usa-capitals-weather/components/modal/modal.component.spec.ts
+++ src/app/modules/usa-capitals-weather/components/modal/modal.component.spec.ts
@@ -1,6 +1,8 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { TranslateModule } from '@ngx-translate/core';
 import { ModalComponent } from './modal.component';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 describe('ModalComponent', () => {
   let component: ModalComponent;
@@ -8,13 +10,17 @@ describe('ModalComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        TranslateModule.forRoot(),
+        MatProgressSpinnerModule,
+        HttpClientTestingModule
+       ],
       declarations: [ ModalComponent ]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(ModalComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
--- src/app/modules/usa-capitals-weather/usa-capitals-weather.module.ts
+++ src/app/modules/usa-capitals-weather/usa-capitals-weather.module.ts
@@ -1,31 +1,24 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
-import { ReactiveFormsModule } from '@angular/forms';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { TranslateModule } from '@ngx-translate/core';
+import { SharedModule } from 'src/app/shared/module/shared.module';
 import { ModalComponent } from './components/modal/modal.component';
 import { UsaCapitalsWeatherComponent } from './components/usa-capitals-weather/usa-capitals-weather.component';
 import { UsaCapitalsWeatherRoutingModule } from './usa-capitals-weather-routing.module';
-import { OverlayModule } from '@angular/cdk/overlay';
-import { SharedModule } from 'src/app/shared/module/shared.module';
 
 
 @NgModule({
   declarations: [
-    UsaCapitalsWeatherComponent,
-    ModalComponent
+    ModalComponent,
+    UsaCapitalsWeatherComponent
   ],
   imports: [
     CommonModule,
-    // TranslateModule,
-    // ReactiveFormsModule,
-    // OverlayModule,
-    // MatProgressSpinnerModule,
-    // MatDialogModule,
     SharedModule,
     UsaCapitalsWeatherRoutingModule
+  ],
+  exports: [
+    ModalComponent
   ]
 })
 export class UsaCapitalsWeatherModule { }
